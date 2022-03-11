bf947cd64cdee90f830594458d14706488248618
Fasica37 <fasica.zewdie7@gmail.com>
2022-03-11 09:03:29 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from './add-product/add-product.component';
+import { CustomerComponent } from './customer/customer.component';
+import { EditProductComponentComponent } from './edit-product-component/edit-product-component.component';
+import { ProductComponent } from './product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'products/edit/:id',
+component: EditProductComponentComponent
+},
+{path: 'products',
+component: ProductComponent
+},{path: 'customers',
+component: CustomerComponent
+},{path: 'products/add',
+component: AddProductComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { FormsModule } from '@angular/forms';
+import { CustomerComponent } from './customer/customer.component';
+import { EditProductComponentComponent } from './edit-product-component/edit-product-component.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    CustomerComponent,
+    EditProductComponentComponent,
+    AddProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
